8681b26569de79bf1b00fcb790ecd69256056009
Roout <following2future@gmail.com>
2020-05-24 11:03:01 +0300

    Successful import of the Dragon Bones animation

--- Classes/UnitView.cpp
+++ Classes/UnitView.cpp
@@ -2,6 +2,10 @@
 #include "Unit.hpp"
 #include "PhysicWorld.hpp"
 
+#include "dragonBones/DragonBonesHeaders.h"
+#include "dragonBones/cocos2dx/CCDragonBonesHeaders.h"
+
+
 HeroView * HeroView::create(const Unit* const model) {
     auto pRet = new (std::nothrow) HeroView(model);
     if (pRet && pRet->init()) {
@@ -22,17 +26,27 @@ bool HeroView::init() {
     const auto body { m_model->GetBody() };
     const auto shape { body->GetShape() };
     this->setPosition(shape.origin);
-    this->drawSolidRect(cocos2d::Vec2::ZERO, shape.size, cocos2d::Color4F::MAGENTA );
-
-    auto label = cocos2d::Label::createWithSystemFont(
-        (body->IsOnGround()? "ground": "no ground"), 
-        "Arial", 
-        18
-    );
-    this->addChild(label);
-    label->setPosition(cocos2d::Vec2(shape.size.width / 2.f, shape.size.height + 5.f));
-    label->setName("info");
     
+    auto factory = dragonBones::CCFactory::getFactory();
+    factory->loadDragonBonesData("axe-warrior-3/walk_ske.json");
+    factory->loadTextureAtlasData("axe-warrior-3/walk_tex.json");
+    auto armatureDisplay = factory->buildArmatureDisplay("Armature", "walk");
+    armatureDisplay->setScale( 0.2f );
+    // factory->loadDragonBonesData("mecha_1002_101d_show/mecha_1002_101d_show_ske.dbbin");
+    // factory->loadTextureAtlasData("mecha_1002_101d_show/mecha_1002_101d_show_tex.json");
+    // const auto armatureDisplay = factory->buildArmatureDisplay("mecha_1002_101d", "mecha_1002_101d_show");
+    armatureDisplay->getAnimation()->play("walk");
+    armatureDisplay->setAnchorPoint(cocos2d::Vec2::ANCHOR_BOTTOM_LEFT);
+    // this->setScaleX(-1.f);
+    // armatureDisplay->setScaleX(-1.f);
+
+
+    this->addChild(armatureDisplay);
+    const auto box { armatureDisplay->getBoundingBox() }; 
+    // this->drawSolidRect(cocos2d::Vec2::ZERO, shape.size, cocos2d::Color4F::MAGENTA );
+    this->drawRect(cocos2d::Vec2::ZERO , box.size, cocos2d::Color4F::MAGENTA );
+
+    // armatureDisplay->getAnimation()->play("walk");
     return true;
 }
 
@@ -40,10 +54,9 @@ void HeroView::update(float dt) {
     const auto body { m_model->GetBody() };
     const auto shape { body->GetShape() };
     this->setPosition(shape.origin);
-    auto label = (cocos2d::Label*)this->getChildByName("info");
-    label->setString(body->IsOnGround()? "ground": "no ground");
+    
 }
  
-HeroView::HeroView(const Unit* model): 
+HeroView::HeroView(const Unit* model) : 
     m_model { model } 
 {}
