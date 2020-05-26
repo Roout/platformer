9b773defa802f4a0e480a123fdd231a86adf77ab
Roout <following2future@gmail.com>
2020-05-26 11:56:35 +0300

    Add dragon bones animation for unit: - movement animation with x-axis flipping  to change direction.

--- Classes/UnitView.cpp
+++ Classes/UnitView.cpp
@@ -1,5 +1,4 @@
 #include "UnitView.hpp"
-#include "Unit.hpp"
 #include "PhysicWorld.hpp"
 
 #include "dragonBones/DragonBonesHeaders.h"
@@ -23,40 +22,133 @@ bool HeroView::init() {
         return false;
     }
     this->scheduleUpdate();
+
     const auto body { m_model->GetBody() };
     const auto shape { body->GetShape() };
-    this->setPosition(shape.origin);
+    this->setAnchorPoint(cocos2d::Vec2::ANCHOR_MIDDLE_BOTTOM);
+    this->setPosition(shape.origin + cocos2d::Vec2{ shape.size.width / 2.f, 0.f });
     
-    auto factory = dragonBones::CCFactory::getFactory();
-    factory->loadDragonBonesData("axe-warrior-3/walk_ske.json");
-    factory->loadTextureAtlasData("axe-warrior-3/walk_tex.json");
+    // load animation data and build the armature
+    const auto factory = dragonBones::CCFactory::getFactory();
+    factory->loadDragonBonesData("axe-warrior/walk_ske.json");
+    factory->loadTextureAtlasData("axe-warrior/walk_tex.json");
     auto armatureDisplay = factory->buildArmatureDisplay("Armature", "walk");
-    armatureDisplay->setScale( 0.2f );
-    // factory->loadDragonBonesData("mecha_1002_101d_show/mecha_1002_101d_show_ske.dbbin");
-    // factory->loadTextureAtlasData("mecha_1002_101d_show/mecha_1002_101d_show_tex.json");
-    // const auto armatureDisplay = factory->buildArmatureDisplay("mecha_1002_101d", "mecha_1002_101d_show");
-    armatureDisplay->getAnimation()->play("walk");
-    armatureDisplay->setAnchorPoint(cocos2d::Vec2::ANCHOR_BOTTOM_LEFT);
-    // this->setScaleX(-1.f);
-    // armatureDisplay->setScaleX(-1.f);
 
-
+    // TODO: scale factor depends on device resolution so it can'be predefined constant.
+    constexpr auto scaleFactor { 0.2f };
+    
+    armatureDisplay->setName("Armature");
+    // armatureDisplay->setAnchorPoint(cocos2d::Vec2::ANCHOR_MIDDLE_BOTTOM);
+    
     this->addChild(armatureDisplay);
-    const auto box { armatureDisplay->getBoundingBox() }; 
-    // this->drawSolidRect(cocos2d::Vec2::ZERO, shape.size, cocos2d::Color4F::MAGENTA );
-    this->drawRect(cocos2d::Vec2::ZERO , box.size, cocos2d::Color4F::MAGENTA );
 
-    // armatureDisplay->getAnimation()->play("walk");
+    // adjust animation
+    armatureDisplay->setScale( scaleFactor );
+    armatureDisplay->getAnimation()->play("walk");
+
+    // debug
+    const auto box { armatureDisplay->getBoundingBox() }; 
+    this->drawRect( 
+        cocos2d::Vec2 { -box.size.width / 2.f, 0.f }, 
+        cocos2d::Vec2 { box.size.width / 2.f, box.size.height }, 
+        cocos2d::Color4F::MAGENTA 
+    );
+    this->drawRect( 
+        cocos2d::Vec2 { -shape.size.width / 2.f, 0.f }, 
+        cocos2d::Vec2 { shape.size.width / 2.f, shape.size.height }, 
+        cocos2d::Color4F::YELLOW
+    );
     return true;
 }
 
+inline std::string CreateAnimationName( Unit::State state) {
+    std::string animationName { "walk" };
+    switch (state)
+    {
+        case Unit::State::idle: break;
+        case Unit::State::jump: break;
+        case Unit::State::move: break;
+        case Unit::State::attack: break; 
+        default: break;
+    }
+    return animationName;
+}
+
+void HeroView::FlipX(const cocos2d::Vec2& currentDir) {
+    auto armatureDisplay = dynamic_cast<dragonBones::CCArmatureDisplay*>(
+        this->getChildByName("Armature")
+    );
+
+    if(const auto isTurnedRight = armatureDisplay->getArmature()->getFlipX(); 
+        isTurnedRight && currentDir.x < 0.f
+    ) {
+        armatureDisplay->getArmature()->setFlipX(false);
+    } 
+    else if( !isTurnedRight && currentDir.x > 0.f) { 
+        armatureDisplay->getArmature()->setFlipX(true);
+    }
+}
+
+void HeroView::UpdateAnimation() {
+    const auto body { m_model->GetBody() };
+
+    auto armatureDisplay = dynamic_cast<dragonBones::CCArmatureDisplay*>(
+        this->getChildByName("Armature")
+    );
+    
+    const auto currentState { m_model->GetState() };
+    const auto currentDir   { body->GetDirection() };
+
+    if( currentState == m_lastState.state ) 
+    { // state is same, checking for state details
+        if( m_lastState.direction.x != currentDir.x ) 
+        { // direction has been changed, adjust animation:
+            if( currentDir.x != 0.f ) {
+                // unit moving in another direction
+                this->FlipX(currentDir);
+            } else {
+                // the model doesn't move along x-Axis but stayed still!
+                // TODO: add another animation
+            }
+            // update last state:
+            m_lastState.direction = currentDir;
+        }
+        // otherwise directions are equel and nothing really changed!
+    } else { // state has been changed.
+        // adjust view to the new state
+        if( currentState == Unit::State::move) {
+            // invert the image if needed
+            if( ( currentDir.x > 0.f && m_lastState.direction.x <= 0.f ) ||
+                ( currentDir.x < 0.f && m_lastState.direction.x >= 0.f )
+            ) {
+                this->FlipX(currentDir);
+            }
+        }
+
+        // update last state
+        m_lastState.state       = currentState;
+        m_lastState.direction   = currentDir;
+        // run animation
+        const std::string animationName { CreateAnimationName(currentState) };
+        armatureDisplay->getAnimation()->play(animationName);
+    }
+}
+
 void HeroView::update(float dt) {
+    // Update animation
+    this->UpdateAnimation();
+
     const auto body { m_model->GetBody() };
     const auto shape { body->GetShape() };
-    this->setPosition(shape.origin);
-    
+    this->setPosition(shape.origin + cocos2d::Vec2{ shape.size.width / 2.f, 0.f });
 }
  
 HeroView::HeroView(const Unit* model) : 
-    m_model { model } 
-{}
+    m_model { model }
+{
+    const auto body { m_model->GetBody() };
+    // Last update data are similar to current model's data
+    // on initialization.
+    m_lastState.direction = body->GetDirection();
+    m_lastState.state = m_model->GetState();
+}
--- Classes/UnitView.hpp
+++ Classes/UnitView.hpp
@@ -2,8 +2,7 @@
 #define UNIT_VIEW_HPP
 
 #include "cocos2d.h"
-
-class Unit;
+#include "Unit.hpp"
 
 class HeroView final : public cocos2d::DrawNode {
 public:
@@ -16,7 +15,22 @@ public:
 private:
     HeroView(const Unit* model);
 
+    void UpdateAnimation();
+
+    void FlipX(const cocos2d::Vec2& currentDir);
+private:
+
     const Unit * const m_model { nullptr };
+
+    /**
+     * Data from the previous unit state.
+     * Used to monitor the changes of the unit state so that view will also be changed 
+     * with animation and other stuff.
+     */
+    struct LastUpdate {
+        cocos2d::Vec2   direction { 0.f, 0.f };
+        Unit::State     state { Unit::State::idle };
+    } m_lastState {};
 };
 
 #endif // UNIT_VIEW_HPP
