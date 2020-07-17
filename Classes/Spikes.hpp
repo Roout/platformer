bd7fabf5fb6442371a76da5e846a4d6e646c537e
Roout <following2future@gmail.com>
2020-07-17 23:20:29 +0300

    change spike type in the same way as other models were implemented.

--- Classes/Spikes.hpp
+++ Classes/Spikes.hpp
@@ -4,24 +4,22 @@
 #include "cocos2d.h"
 #include "Core.hpp"
 
+
 /**
  * Do damage every m_maxCooldown seconds to each body it can influence
  */
-class Spikes final : public core::Entity {
+class Spikes final {
 public:
-    Spikes(
-        const cocos2d::Vec2& pos, 
-        const cocos2d::Size& size
-    ) {   
-        m_body = cocos2d::PhysicsBody::createBox(size);
+    Spikes( cocos2d::PhysicsBody * const body) 
+        : m_body { body }
+    {   
         m_body->setDynamic(false);
-        m_body->setPositionOffset(pos);
         m_body->setCategoryBitmask(
             core::CreateMask(
                 core::CategoryBits::TRAP
             )
         );
-        m_body->setCollisionBitmask(
+        m_body->setContactTestBitmask(
             core::CreateMask(
                 core::CategoryBits::ENEMY, 
                 core::CategoryBits::HERO
@@ -29,38 +27,12 @@ public:
         );
     }
 
-    ~Spikes() {
-        m_body->removeFromWorld();
-    }
-
-    void Update(const float dt) {
-        if(m_cooldown > 0.f) {
-            m_cooldown -= dt;
-        }
-    }
-
-    bool CanDamage() const noexcept {
-        return m_cooldown <= 0.f;
-    }
-
-    float DealDamage() const noexcept {
-        if( this->CanDamage() ) {
-            m_cooldown = m_maxCooldown;
-            return m_damage;
-        } 
-        else {
-            return 0.f;
-        }
-    }
 
 private:
     
     cocos2d::PhysicsBody * m_body { nullptr };
 
-    float   m_cooldown { 0.f };
-
-    static constexpr float m_maxCooldown { 0.3f };
-    static constexpr float m_damage { 3.f };
+    static constexpr float m_damage { 10.f };
 };
 
 #endif // SPIKES_HPP
