746ad2945afebae1066e9752f558899bf253bc93
Roout <following2future@gmail.com>
2020-09-01 00:33:23 +0300

    refactor(Player): adjust to base class changes, add input handler as private property
    * input handler was depending only on play and has lifetime lesser than player's one
    * all animations are updated in UpdateAnimation method, states - UpdateStates, so their responsibility isn't blurred between other methods

--- Classes/Player.cpp
+++ Classes/Player.cpp
@@ -1,14 +1,18 @@
 #include "Player.hpp"
 #include "SmoothFollower.hpp"
+#include "UserInputHandler.hpp"
 #include "PhysicsHelper.hpp"
 #include "Utils.hpp"
 #include "Core.hpp"
 #include "SizeDeducer.hpp"
 #include "Weapon.hpp"
 #include "DeathScreen.hpp"
+#include "DragonBonesAnimator.hpp"
 
-Player* Player::create(const cocos2d::Size& size) {
-    auto pRet { new (std::nothrow) Player(size) };
+#include <algorithm>
+
+Player* Player::create() {
+    auto pRet { new (std::nothrow) Player() };
     if( pRet && pRet->init()) {
         pRet->autorelease();
     } else {
@@ -18,23 +22,54 @@ Player* Player::create(const cocos2d::Size& size) {
     return pRet;
 }
 
-Player::Player(const cocos2d::Size& sz) :
-    Unit { sz, "mc" }
+Player::Player() :
+    Unit { "mc" }
 {
-    // Create weapon
-    const int damage { 25 };
-    const int range { SizeDeducer::GetInstance().GetAdjustedSize(60) };
-    const float reloadTime { m_maxAttackTime };
-    m_weapon = std::make_unique<Sword>( damage, range, reloadTime );
+    m_designedSize = cocos2d::Size{ 80.f, 135.f };
 }
-
 
 bool Player::init() {
     if( !Unit::init()) {
         return false; 
     }
+    // create weapon (it should be read from config)
+    const auto damage { 25.f };
+    const auto range { 60.f };
+    const auto preparationTime { 0.f };
+    const auto attackDuration { m_animator->GetDuration(Utils::EnumCast(Act::attack)) };
+    const auto reloadTime { 0.1f };
+    m_weapon = std::make_unique<Sword>(
+        damage, 
+        range, 
+        preparationTime,
+        attackDuration,
+        reloadTime 
+    );
+    
     m_follower = std::make_unique<SmoothFollower>(this);
+    m_inputHandler = std::make_unique<UserInputHandler>(this);
+    
+    return true;
+}
 
+void Player::AddAnimator() {
+    std::string chachedArmatureName = m_dragonBonesName;
+    m_animator = dragonBones::Animator::create(std::move(chachedArmatureName));
+    m_animator->InitializeAnimations({
+        std::make_pair(Utils::EnumCast(Act::attack), "attack"),
+        std::make_pair(Utils::EnumCast(Act::dead), "idle"),
+        std::make_pair(Utils::EnumCast(Act::idle), "idle"),
+        std::make_pair(Utils::EnumCast(Act::jump), "jump"),
+        std::make_pair(Utils::EnumCast(Act::move), "walk")
+    });
+    
+    this->addChild(m_animator);
+    m_animator->setScale(0.2f); // TODO: introduce multi-resolution scaling
+}
+
+void Player::AddPhysicsBody(const cocos2d::Size& size) {
+    Unit::AddPhysicsBody(size);
+    
     const auto body { this->getPhysicsBody() };
     body->setCategoryBitmask(
         Utils::CreateMask(core::CategoryBits::HERO)
@@ -67,7 +102,6 @@ bool Player::init() {
             core::CategoryBits::PLATFORM
         )
     );
-    return true;
 }
 
 void Player::setPosition(const cocos2d::Vec2& position) {
@@ -75,18 +109,39 @@ void Player::setPosition(const cocos2d::Vec2& position) {
     m_follower->Reset();
 }
 
+void Player::UpdatePosition(const float dt) noexcept {
+    if(!this->IsDead()) {
+        m_movement.Update(dt);
+        m_follower->UpdateMapPosition(dt);
+    }
+}
+
+void Player::pause() {
+    cocos2d::Node::pause();
+    m_animator->pause();
+    if(!this->IsDead()) {
+        // prevent to being called onExit() when the player is dead and is being detached!
+        m_inputHandler->Reset();
+    }
+}
+
+void Player::resume() {
+    cocos2d::Node::resume();
+    m_animator->resume();
+}
+
 void Player::update(float dt) {
     this->UpdateDebugLabel();
     this->UpdateWeapon(dt);
     this->UpdatePosition(dt); 
-    m_follower->UpdateMapPosition(dt);
     this->UpdateCurses(dt);
-    this->UpdateAnimation(); 
     this->UpdateState(dt);
+    this->UpdateAnimation(); 
 }
 
 void Player::UpdateAnimation() {
     if( m_currentState.m_act == Act::dead ) {
+        // emit particles
         const auto emitter = cocos2d::ParticleSystemQuad::create("particle_texture.plist");
         emitter->setAutoRemoveOnFinish(true);
         /// TODO: adjust for the multiresolution
@@ -94,51 +149,53 @@ void Player::UpdateAnimation() {
         emitter->setPositionType(cocos2d::ParticleSystem::PositionType::RELATIVE);
         emitter->setPosition(this->getPosition());
         this->getParent()->addChild(emitter, 9);
-    } 
-    else {
-        Unit::UpdateAnimation();
-    }
-}
+        // remove physics body
+        this->removeComponent(this->getPhysicsBody());
 
-void Player::UpdateState(const float dt) noexcept {
-    if( m_currentState.m_act == Act::dead ) {
-        m_follower.reset();
-        this->removeFromParent();
-        // show death screen:
+        // create a death screen
         const auto visibleSize = cocos2d::Director::getInstance()->getVisibleSize();
         const auto origin = cocos2d::Director::getInstance()->getVisibleOrigin();
 
         const auto node = DeathScreen::create();
         node->setAnchorPoint(cocos2d::Vec2::ANCHOR_MIDDLE);
-        node->setPosition(origin / 2.f + visibleSize / 2.f );
+        node->setPosition(visibleSize / 2.f);
 
-        const auto scene = cocos2d::Director::getInstance()->getRunningScene();
-        const auto ui = scene->getChildByName("Interface");
+        const auto ui = cocos2d::Director::getInstance()->getRunningScene()->getChildByName("Interface");
         ui->addChild(node);
 
-        return;
-    }
-
-    m_previousState = m_currentState;
-
-    constexpr float EPS { 0.00001f };
-
-    // update character state
-    if( m_currentState.m_act == Act::attack ) {
-        m_attackTime -= dt;
-        if( helper::IsPositive(m_attackTime, EPS) ) {
-            // exit to not change an attack state
-            return; 
+        // remove from screen
+        this->runAction(cocos2d::RemoveSelf::create());
+    } 
+    else if( m_currentState.m_act != m_previousState.m_act ) {
+        int repeatTimes { dragonBones::Animator::INFINITY_LOOP };
+        if( m_currentState.m_act == Act::attack || 
+            m_currentState.m_act == Act::jump 
+        ) {
+            repeatTimes = 1;
         }
+        m_animator->Play(Utils::EnumCast(m_currentState.m_act), repeatTimes);
     }
+}
+
+void Player::UpdateState(const float dt) noexcept {
+    m_previousState.m_act = m_currentState.m_act;
 
     const auto velocity { this->getPhysicsBody()->getVelocity() };
+    static constexpr float EPS { 0.00001f };
 
-    if( !this->IsOnGround() ) {
+    if( m_health <= 0 ) {
+        m_currentState.m_act = Act::dead;
+    }
+    else if( m_weapon->IsAttacking() ) {
+        m_currentState.m_act = Act::attack;
+    }
+    else if( !this->IsOnGround() ) {
         m_currentState.m_act = Act::jump;
-    } else if( helper::IsEquel(velocity.x, 0.f, EPS) ) {
+    } 
+    else if( helper::IsEquel(velocity.x, 0.f, EPS) ) {
         m_currentState.m_act = Act::idle;
-    } else {
+    } 
+    else {
         m_currentState.m_act = Act::move;
     }
 }
--- Classes/Player.hpp
+++ Classes/Player.hpp
@@ -5,29 +5,43 @@
 #include <memory>
 
 class SmoothFollower;
+class UserInputHandler;
 
 class Player final : public Unit {
 public:
     static constexpr char * const NAME = "Player";
 
-    static Player* create(const cocos2d::Size&);
+    static Player* create();
 
     [[nodiscard]] bool init() override;
 
     void update(float dt) override;
 
     void setPosition(const cocos2d::Vec2& position) override;
+    
+    void pause() override;
+
+    void resume() override;
 
 private:
     void UpdateState(const float dt) noexcept override;
 
     void UpdateAnimation() override;
 
-    Player(const cocos2d::Size&);
+    void AddPhysicsBody(const cocos2d::Size&) override;
+
+    void AddAnimator() override;
+
+    void UpdatePosition(const float dt) noexcept override;
+
+    Player();
 
     /// Properties
 private:
     std::unique_ptr<SmoothFollower> m_follower { nullptr };
+
+    // controller:
+    std::unique_ptr<UserInputHandler> m_inputHandler { nullptr };
 };
 
 #endif // PLAYER_HPP
