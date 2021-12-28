cbad3854beb327b271e5f2e03bf609cb159aa3b3
Roout <following2future@gmail.com>
2021-12-28 21:05:12 +0300

    feature: init `FireCloud` constants from json

--- Classes/units/FireCloud.cpp
+++ Classes/units/FireCloud.cpp
@@ -6,13 +6,18 @@
 #include "../Weapon.hpp"
 #include "../Movement.hpp"
 
+#include "../configs/JsonUnits.hpp"
+
 #include "cocos2d.h"
 
 namespace Enemies {
 
-FireCloud* FireCloud::create(size_t id, const cocos2d::Size& contentSize) {
-    auto pRet { new (std::nothrow) FireCloud(id, contentSize) };
-    if( pRet && pRet->init()) {
+FireCloud* FireCloud::create(size_t id
+    , const cocos2d::Size& contentSize
+    , const json_models::UnitsFirecloud *model) 
+{
+    auto pRet { new (std::nothrow) FireCloud(id, contentSize, model) };
+    if (pRet && pRet->init()) {
         pRet->autorelease();
     } 
     else {
@@ -22,9 +27,14 @@ FireCloud* FireCloud::create(size_t id, const cocos2d::Size& contentSize) {
     return pRet;
 }
 
-FireCloud::FireCloud(size_t id, const cocos2d::Size& contentSize)
+FireCloud::FireCloud(size_t id
+    , const cocos2d::Size& contentSize
+    , const json_models::UnitsFirecloud *model
+)
     : Bot{ id, core::EntityNames::FIRECLOUD }
+    , m_model { model }
 {
+    assert(model);
     m_contentSize = cocos2d::Size { contentSize.width, contentSize.height * 2.f };
     m_physicsBodySize = m_contentSize;
     m_hitBoxSize = m_physicsBodySize;
@@ -35,13 +45,13 @@ bool FireCloud::init() {
         return false; 
     }
 
-    if(auto healthBar = this->getChildByName("health"); healthBar) {
+    if (auto healthBar = getChildByName("health"); healthBar) {
         healthBar->removeFromParent();
     }
 
-    m_health = 100; // some big value to make cloud indestructable
-    // set up lifetime of the cloud
-    m_lifetime = CLOUD_LIFETIME;
+    m_health = m_model->health; 
+    m_lifetime = m_model->lifetime;
+    m_shells = m_model->shellRefillCount;
 
     return true;
 }
@@ -77,17 +87,17 @@ void FireCloud::UpdatePosition(const float dt) noexcept {
 
 void FireCloud::UpdateWeapons(const float dt) noexcept {
     Unit::UpdateWeapons(dt);
-    if(m_shells <= 0.f) {
+    if (m_shells <= 0.f) {
         m_shellRenewTimer -= dt;
-        if(m_shellRenewTimer <= 0.f) {
-            m_shells = MAX_SHELL_COUNT;
+        if (m_shellRenewTimer <= 0.f) {
+            m_shells = m_model->shellRefillCount;
         }
     }
 }
 
 void FireCloud::TryAttack() {
-    if (this->NeedAttack()) { // attack if possible
-        this->Attack();
+    if (NeedAttack()) { // attack if possible
+        Attack();
     } 
 }
 
@@ -111,7 +121,7 @@ void FireCloud::UpdateState(const float dt) noexcept {
             Stop(Movement::Axis::Y);
             assert(getPhysicsBody() && "Cloud doesn't have physics body");
 
-            m_movement->SetMaxSpeed(CLOUD_SPEED);
+            m_movement->SetMaxSpeed(m_model->maxSpeed);
             using Move = Movement::Direction;
             m_movement->Push(IsLookingLeft()? Move::LEFT: Move::RIGHT);
         }
@@ -119,20 +129,20 @@ void FireCloud::UpdateState(const float dt) noexcept {
 }
 
 void FireCloud::UpdateAnimation() {
-    if(m_currentState != m_previousState) {
+    if (m_currentState != m_previousState) {
         auto isOneTimeAttack { m_currentState != State::LATE };
         auto repeatTimes { isOneTimeAttack ? 1 : dragonBones::Animator::INFINITY_LOOP };
         (void) m_animator->Play(Utils::EnumCast(m_currentState), repeatTimes);
-        if(m_currentState == State::DEAD) {
-            this->OnDeath();
+        if (m_currentState == State::DEAD) {
+            OnDeath();
         }
     }
 }
 
 void FireCloud::OnDeath() {
-    m_animator->EndWith([this](){
-        this->removeComponent(this->getPhysicsBody());
-        this->runAction(cocos2d::RemoveSelf::create(true));
+    m_animator->EndWith([this]() {
+        removeComponent(getPhysicsBody());
+        runAction(cocos2d::RemoveSelf::create(true));
     });
 }
 
@@ -150,7 +160,7 @@ void FireCloud::AddPhysicsBody() {
     body->setCategoryBitmask(0);
     body->setContactTestBitmask(0);
 
-    this->addComponent(body);
+    addComponent(body);
 }
 
 void FireCloud::AddAnimator() {
@@ -165,18 +175,15 @@ void FireCloud::AddAnimator() {
         std::make_pair(Utils::EnumCast(State::LATE),    GetStateName(State::LATE)),
         std::make_pair(Utils::EnumCast(State::DEAD),    GetStateName(State::LATE))
     });
-    m_animator->EndWith([this](){
-       this->m_finished = true;
+    m_animator->EndWith([this]() {
+       m_finished = true;
     });
-    this->addChild(m_animator);
+    addChild(m_animator);
 }
 
 void FireCloud::AddWeapons() {
-    const auto damage { 15.f };
-    const auto range { 115.f };
     const auto preparationTime { 0.f }; 
     const auto attackDuration { 0.1f };
-    const auto reloadTime { 0.1f };
 
     auto genPos = [this]()->cocos2d::Rect {
         auto attackRange { m_weapons[WeaponClass::RANGE]->GetRange() };
@@ -194,19 +201,24 @@ void FireCloud::AddWeapons() {
         return { position, fireballSize };
     };
     auto genVel = [this](cocos2d::PhysicsBody* body) {
-        body->setVelocity({ IsLookingLeft()? -300.f: 300.f, -600.f });
+        const auto& velocity = m_model->weapons.fireball.projectile.velocity;
+        body->setVelocity({ IsLookingLeft()? -velocity[0]: velocity[0], -velocity[1] });
     };
 
+    const auto& fireball = m_model->weapons.fireball;
     auto& weapon = m_weapons[WeaponClass::RANGE];
-    weapon.reset(new CloudFireball(
-        damage, range, preparationTime, attackDuration, reloadTime));
+    weapon.reset(new CloudFireball(fireball.projectile.damage
+        , fireball.range
+        , preparationTime
+        , attackDuration
+        , fireball.cooldown));
     weapon->AddPositionGenerator(std::move(genPos));
     weapon->AddVelocityGenerator(std::move(genVel));
 }
 
 void FireCloud::Attack() {
     if (--m_shells <= 0) {
-        m_shellRenewTimer = FireCloud::MAX_SHELL_DELAY;
+        m_shellRenewTimer = m_model->shellRefillCooldown;
     }
     m_weapons[WeaponClass::RANGE]->LaunchAttack();
 }
